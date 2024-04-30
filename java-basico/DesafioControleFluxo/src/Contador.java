6a53b52b07e35e0598ad04e398be4944a2a5145e
israel-freire14 <israel@israel.israel>
2024-04-30 13:21:44 -0300

    Desafio Controle de Fluxo - Contador

--- java-basico/DesafioControleFluxo/src/Contador.java
+++ java-basico/DesafioControleFluxo/src/Contador.java
@@ -1,6 +1,5 @@
-package Desafio;
-import java.nio.channels.ScatteringByteChannel;
 import java.util.Scanner;
+
 
 
 public class Contador {
@@ -8,15 +7,14 @@ public class Contador {
         Scanner terminal = new Scanner(System.in);
 
         System.out.printf("Digite o primeiro parâmetro: ");
-            int parametroUm = Integer.parseInt(terminal.nextLine());
+        int parametroUm = Integer.parseInt(terminal.nextLine());
         System.out.printf("Digite o segundo parâmetro: ");
-            int parametroDois = Integer.parseInt(terminal.nextLine());
+        int parametroDois = Integer.parseInt(terminal.nextLine());
         terminal.close();
 
-        try{
-            contar(parametroUm,parametroDois);
-        }
-        catch (ParametrosInvalidosException e){
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
             System.out.println("===============================================================");
             System.out.println("UM DOS PARÂMETROS NÃO CORRESPONDEM COM A LÓGICA DE NEGÓCIO");
             System.out.println("===============================================================");
@@ -24,22 +22,25 @@ public class Contador {
 
         }
 
-
     }
 
-    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        if(parametroUm > parametroDois){
+        if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
-        } else{
+        } else {
             int contagem = parametroDois - parametroUm;
 
             System.out.println("============================");
 
-            for(int i = 1; i<contagem; i++){
-                System.out.println("Imprimindo o número "+i);
+            for (int i = 1; i < contagem; i++) {
+                System.out.println("Imprimindo o número " + i);
             }
             System.out.println("============================");
         }
     }
 }
+
+
+
+
--- java-basico/DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ java-basico/DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,5 +1,3 @@
-package Desafio;
-
 public class ParametrosInvalidosException extends Exception {
 
 }
