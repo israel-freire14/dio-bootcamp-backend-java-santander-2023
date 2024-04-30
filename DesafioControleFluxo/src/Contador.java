c9bfae4c5eaeee80f881b77d01ae2c9ff4b9c060
israel-freire14 <israelfreire999@gmail.com>
2024-04-30 14:07:52 -0300

    Organizando repositório

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -18,19 +18,15 @@ public class Contador {
             System.out.println("===============================================================");
             System.out.println("UM DOS PARÂMETROS NÃO CORRESPONDEM COM A LÓGICA DE NEGÓCIO");
             System.out.println("===============================================================");
-
-
         }
 
     }
-
     public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         } else {
             int contagem = parametroDois - parametroUm;
-
             System.out.println("============================");
 
             for (int i = 1; i < contagem; i++) {
